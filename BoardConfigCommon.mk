b914466cb7472a40e356a8216d9fb3efd6ff8823
Captain Throwback <captainthrowback@hotmail.com>
2023-08-25 14:24:36 -0400

    sm8350-common: remove unneeded items
    - libxml2 is included upstream
    - move framerate flag to device trees
    
    Change-Id: I79e434cf252b879f417e5f3e382ae5e14eb75ba2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -99,7 +99,6 @@ BOARD_USES_RECOVERY_AS_BOOT := true
 TARGET_NO_RECOVERY := true
 TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
-    libxml2 \
     vendor.display.config@1.0 \
     vendor.display.config@2.0
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/recovery.fstab
@@ -141,7 +140,6 @@ TW_MAX_BRIGHTNESS := 1024
 TW_EXCLUDE_APEX := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
-TW_FRAMERATE := 90
 TW_INCLUDE_CRYPTO := true
 TW_NO_EXFAT_FUSE := true
 TW_INCLUDE_REPACKTOOLS := true
@@ -150,7 +148,6 @@ TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.date.utc;ro.bootimage.build.date.utc=ro.build.date.utc;ro.odm.build.date.utc=ro.build.date.utc;ro.product.build.date.utc=ro.build.date.utc;ro.system.build.date.utc=ro.build.date.utc;ro.system_ext.build.date.utc=ro.build.date.utc;ro.vendor.build.date.utc=ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
 
